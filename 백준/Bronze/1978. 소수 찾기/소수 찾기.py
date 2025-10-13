b6d049ad511076e4c8fb4d8d9f212348cae97c7d
zktmtls1 <zktmtls1@gmail.com>
2025-10-14 08:40:15 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,20 +1,16 @@
 N = int(input())
-array1 = []
-array1 = list(map(int, input().split()))
-count =0
+nums = list(map(int, input().split()))
+count = 0
 
-for i in range (0,N):
-    temp = array1[i]
-    condition = True
-    if (temp==1):
-        condition = False
-    elif (temp==2 or temp==3):
-        condition = False
-        count+=1
-    for j in range (2,int(temp**0.5)+1):
-        if(temp%j==0): 
-            condition = False
+for x in nums:
+    if x < 2:
+        continue
+    j = 2
+    while j * j <= x:
+        if x % j == 0:
             break
-    if(condition):
-        count +=1
+        j += 1
+    else:
+        count += 1
+
 print(count)
