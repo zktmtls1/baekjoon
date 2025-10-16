7c718d46d87cdb76b7b6bd57def171b5922b04da
zktmtls1 <zktmtls1@gmail.com>
2025-10-16 09:34:12 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,21 +1,16 @@
-N, M = map(int, input().split())
-array1 = sorted(map(int, input().split()), reverse=False)
-temp=0
+import sys
 
-while(True):
-    for i in range(0, len(array1) - 2):
-        if (array1[i]>M-2):
-            continue
-        else:
-            for j in range(i+1, len(array1)-1):
-                if (array1[i]+array1[j]>M-1):
-                    continue
-                else:
-                    for k in range(j+1, len(array1)):
-                        if (array1[i]+array1[j]+array1[k]>M):
-                            continue
-                        else:
-                            if(array1[i]+array1[j]+array1[k]>temp):
-                                temp=array1[i]+array1[j]+array1[k]
-    print(temp)
-    break
+N, M = map(int, input().split())
+array1 = sorted(map(int, input().split()))
+temp = 0
+
+for i in range(N - 2):
+    for j in range(i + 1, N - 1):
+        for k in range(j + 1, N):
+            s = array1[i] + array1[j] + array1[k]
+            if s == M:
+                print(M)
+                sys.exit(0)   # 전체 종료
+            if s < M and s > temp:
+                temp = s
+print(temp)
